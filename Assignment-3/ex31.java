6c365e66b13fbfa4f0edf5fb87c62f86bf052529
Levindo Neto <levindogtn@gmail.com>
2017-10-17 12:47:03 +0200

    Fix the exercise 3.1 of the assignment 03

--- Assignment-3/ex31.java
+++ Assignment-3/ex31.java
@@ -29,15 +29,18 @@
 public class ex31 {
     public static void main(String[] args) {
 		for (int i=1; i<=100; i++) {
-			if (i%5 == 0) {
+			if ((i%5 == 0) && (i%3 != 0)) {
 				System.out.println("buzz");
 			}
-			if (i%3 == 0) {
+			if ((i%5 != 0) && (i%3 == 0)) {
 				System.out.println("fizz");
 			}
+			else if ((i%5 == 0) && (i%3 == 0)) {
+				System.out.println("fizzbuzz");
+			} 
 			else {
 				System.out.println(i);
-			} 
+			}
 		}
     }
 }
